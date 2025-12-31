334586795ff5b55139815435d4135ba2d75729f8
Yash Parekh <yash.parekh@sap.com>
2025-12-31 15:28:07 +0000

    BE-Authorizations

--- srv/Header_srv.js
+++ srv/Header_srv.js
@@ -7,13 +7,13 @@ module.exports = cds.service.impl(srv => {
 
   async function me(req) { return await cds.run(SELECT.one.from(Header).where({ empNo: req.user.id })); }
 
-  // srv.on('READ', 'LeaveRequests', async (req, next) => {
-  //   const emp = await me(req); if (!emp) return req.reject(403, 'Unknown employee identity');
-  //   if (has(req, 'HR')) return next();
-  //   if (has(req, 'Manager')) { req.query.where(`employee_ID.manager_ID = '${emp.ID}' OR employee_ID = '${emp.ID}'`); return next(); }
-  //   if (has(req, 'Employee')) { req.query.where({ employee_ID: emp.ID }); return next(); }
-  //   req.reject(403, 'Unauthorized');
-  // });
+  srv.on('READ', 'LeaveRequests', async (req, next) => {
+    const emp = await me(req); if (!emp) return req.reject(403, 'Unknown employee identity');
+    if (has(req, 'HR')) return next();
+    if (has(req, 'Manager')) { req.query.where(`employee_ID.manager_ID = '${emp.ID}' OR employee_ID = '${emp.ID}'`); return next(); }
+    if (has(req, 'Employee')) { req.query.where({ employee_ID: emp.ID }); return next(); }
+    req.reject(403, 'Unauthorized');
+  });
 
   srv.on('READ', 'Me', async (req) => {
     const emp = await me(req);
